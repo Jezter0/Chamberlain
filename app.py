6db46993cda1bf4751d46d02bf75dd84ae69e4a7
Jason <132189550+Jezter0@users.noreply.github.com>
2024-06-16 16:01:22 +0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, render_template, redirect, url_for, flash
+import bcrypt
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key_here'
+
+users = {}
+
+@app.route('/')
+def index():
+    return render_template('index.html')
